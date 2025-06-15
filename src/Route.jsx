b2b460e7c5ddcc6e5420c87ec5c12765e8bd2459
sarthakkeche <kechesarthak412@gmail.com>
2025-06-15 18:07:13 +0530

    Updated navbar and editorial board with active tab logic

--- src/Route.jsx
+++ src/Route.jsx
@@ -2,12 +2,14 @@ import { useRoutes } from "react-router-dom";
 import Home from './Components/Home';
 import AboutUsContent from "./Components/About";
 import ContactUsContent  from "./Components/ContactUs";
-import Nav from "./Components/Nav";
+import EditorialBoard from "./Components/Eboard";
+
 const RoutesConfig = () => {
   return useRoutes([
     { path: "/", element: <Home /> },
     { path:"about", element:<AboutUsContent/>},
     { path:"contact",element:<ContactUsContent/>},
+    {path:"Eboard",element:<EditorialBoard/>},
     
     
 
